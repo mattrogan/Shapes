ec67b06537f07893150ac5fcbdf1da9b5b9014b6
Matt Rogan <matt.rogan2@gmail.com>
2022-06-07 11:08:54 +0100

    Initial commit

---
+++ Shapes/Point.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Shapes
+{
+	public class Point
+	{
+		private float _x, _y;
+
+		public Point()
+		{
+			_x = 0;
+			_y = 0;
+		}
+
+		public Point(float x, float y)
+        {
+			this._x = x;
+			this._y = y;
+        }
+
+		public void Move(float dx, float dy)
+        {
+			_x += dx;
+			_y += dy;
+        }
+
+		public void Scalar(float s)
+        {
+			_x *= s;
+			_y *= s;
+        }
+	}
+}
+
---
+++ Shapes/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Shapes
+{
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Point p1 = new Point(3, 2);
+            Point p2 = new Point(1, -1);
+            Point origin = new Point();
+        }
+    }
+}
