ad78a8e0e8ca1a886c13e11806e277d93fcf0e1b
mattrogan <Matt.Rogan2@Gmail.com>
2022-06-13 22:44:04 +0100

    Completed Circle class

---
+++ Shapes/Circle.cs
@@ -0,0 +1,46 @@
+﻿using System;
+namespace Shapes
+{
+	public class Circle : Shape
+	{
+
+		public const double PI = 3.14159;
+
+		protected double _radius;
+
+		public Circle()
+        {
+			_radius = 1.0;
+        }
+
+		public Circle(double radius)
+        {
+			_radius = radius;
+        }
+
+		public Circle(double radius, string colour, bool filled)
+        {
+			_filled = filled;
+			_colour = colour;
+			_radius = radius;
+        }
+
+        public double Radius
+        {
+			set; get;
+        }
+
+
+		public double getArea() { return PI * _radius * _radius; }
+
+		public double getPerimeter() { return 2 * PI * _radius; }
+
+		public override String ToString()
+        {
+			return "A Circle with radius=" + _radius + ", which is a subclass of " + base.ToString();
+		}
+
+
+	}
+
+}
--- Shapes/Program.cs
+++ Shapes/Program.cs
@@ -5,10 +5,13 @@ namespace Shapes
 
     public class Program
     {
-        public static void Main(string[] args)
+
+        public static void Main()
         {
             Shape myShape = new("black", true);
             Console.WriteLine(myShape);
+            Circle myCirc = new(5.0, "red", true);
+            Console.WriteLine(myCirc);
         }
     }
 }
