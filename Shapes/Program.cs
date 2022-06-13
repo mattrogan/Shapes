533ffa97dc2c47eac9ad83d45ead82621b57dc32
mattrogan <Matt.Rogan2@Gmail.com>
2022-06-13 23:28:52 +0100

    Simplified constructors

--- Shapes/Program.cs
+++ Shapes/Program.cs
@@ -8,13 +8,13 @@ namespace Shapes
 
         public static void Main()
         {
-            Circle myCirc = new Circle(3.0, "black", true);
+            Circle myCirc = new(3.0, "black", true);
             myCirc.Radius = 5.0;
             Console.WriteLine(myCirc);
 
             Console.WriteLine();
 
-            Rectangle myRect = new Rectangle(3.0, 4.0);
+            Rectangle myRect = new(3.0, 4.0);
             Console.WriteLine(myRect);
         }
     }
