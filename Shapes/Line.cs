e4026e7aafa228c5939ea2ac452f06206af7145c
mattrogan <Matt.Rogan2@Gmail.com>
2022-06-14 10:07:49 +0100

    Added Line class

---
+++ Shapes/Line.cs
@@ -0,0 +1,42 @@
+﻿using System;
+namespace Shapes
+{
+	public class Line
+	{
+		protected Point _start, _end;
+		protected String _colour = "black";
+
+		public Line(Point start, Point end)
+		{
+			_start = start;
+			_end = end;
+
+		}
+
+		public String Colour
+        {
+			get => _colour;
+			set
+			{ _colour = value; }
+        }
+
+		public Point Start
+        {
+			get => _start;
+			set { _start = value; }
+        }
+
+		public Point End
+		{
+			get => _end;
+			set { _end = value; }
+		}
+
+		public override String ToString()
+        {
+			return "A Line starting at " + Start.ToString()
+				+ " and ending at " + End.ToString();
+        }
+	}
+}
+
